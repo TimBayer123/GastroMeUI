566b522d787d8ff5d4092a924773dff189faa8b4
TimRiebesam <tim.riebesam@aol.de>
2020-05-12 20:25:17 +0200

    globals für api-connection angelegt.

--- lib/settings/globals.dart
+++ lib/settings/globals.dart
@@ -2,3 +2,7 @@ library gastrome;
 
 int globalIndex = 2;
 bool loggedIn = false;
+
+final String gastroMeApiUrl = "http://10.0.2.2:5000";
+final String gastroMeApiAuthTokenName = 'gastrome-api-auth-token';
+final String gastroMeApiAuthTokenValue = '4df6d7b9-ba79-4ae7-8a1c-cffbb657610a';
